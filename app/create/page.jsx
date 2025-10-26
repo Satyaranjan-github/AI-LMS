7dfca279eb77310b1bd1b46a1e8ed083c15e3c74
Satyaranjan Sahoo <gudu44791@gmail.com>
2025-10-26 20:01:17 +0530

    Implement Schema and configured course generation through gemini

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,29 +1,54 @@
 "use client"
 
+import { useUser } from "@clerk/nextjs"
+import axios from "axios"
+import { Loader } from "lucide-react"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
+import { v4 as uuidv4 } from "uuid"
 import { Button } from "../../components/ui/button"
 import SelectOptions from "./_components/SelectOptions"
 import TopicInput from "./_components/TopicInput"
 
 function Create() {
+    // to get authUser
+    const { user } = useUser()
+    const [loading, setLoading] = useState(false)
     const [step, setStep] = useState(0)
     const [formData, setFormData] = useState([])
+
+    const router = useRouter()
+
     const handleUserInput = (fieldName, fieldValue) => {
         setFormData((prev) => ({
             ...prev,
             [fieldName]: fieldValue
         })
-
         )
         console.log(formData)
     }
+
+    //Used to save user input and generate AI course layout using ai
+    const GenerateCourseOutline = async () => {
+        const courseId = uuidv4();
+        setLoading(true)
+        const result = await axios.post("/api/generate-course-outline", {
+            courseId: courseId,
+            ...formData,
+            createdBy: user?.primaryEmailAddress?.emailAddress
+        })
+        setLoading(false)
+        router.replace("/dashboard")
+        console.log("Result from create page=>", result.data.result.resp)
+    }
+
     return (
         <div className="flex flex-col items-center p-5 md:px-24 lg:px-36 mt-20">
             <h2 className="font-bold text-4xl text-primary">Start Building Your Personal Study Material</h2>
             <p className="text-gray-500 text-lg">Fill All details in order to generate study material for your next project</p>
             <div className="mt-10 ">
                 {step == 0 ?
-                    <SelectOptions selectStudyType={(value) => handleUserInput("studyType", value)} /> : <TopicInput setTopic={(value) => handleUserInput("topic", value)}
+                    <SelectOptions selectCourseType={(value) => handleUserInput("courseType", value)} /> : <TopicInput setTopic={(value) => handleUserInput("topic", value)}
                         setDifficultyLevel={(value) => handleUserInput("difficultyLevel", value)}
                     />
                 }
@@ -38,8 +63,11 @@ function Create() {
                     <Button onClick={() => setStep(step + 1)}>
                         Next
                     </Button>
-                    : <Button>
-                        Generate
+                    :
+                    <Button onClick={GenerateCourseOutline}
+                        disabled={loading}
+                    >
+                        {loading ? <Loader className="animate-spin" /> : "Generate"}
                     </Button>
                 }
             </div>
