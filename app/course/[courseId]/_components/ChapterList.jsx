a2f60b07bd5160484724d0e4ba53f9feee7e8ea8
Satyaranjan Sahoo <gudu44791@gmail.com>
2025-11-02 09:53:07 +0530

    Implemented Individual Course Page.

---
+++ app/course/[courseId]/_components/ChapterList.jsx
@@ -0,0 +1,23 @@
+
+const ChapterList = ({ course }) => {
+    const chapters = course?.courseLayout?.chapters
+
+    return (
+        <div className='mt-5'>
+            <h2 className='font-medium text-xl'>Chapters</h2>
+            <div className="mt-5">
+                {chapters?.map((chapter, index) => (
+                    <div key={index} className="flex gap-5 items-center p-4 border shadow-md mb-2 rounded-lg cursor-pointer">
+                        <h2 className='text-2xl'>{chapter.emoji}Emoji</h2>
+                        <div >
+                            <h2 className="font-medium">{chapter?.chapterTitle}</h2>
+                            <p className="text-gray-400 text-sm">{chapter?.chapterSummary}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default ChapterList
